ea889b00eebf87f7b9450198f8603dde4b790d86
DSYS <dinodimanjith404@gmail.com>
2025-06-29 19:27:29 +0530

    collection page complete

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,20 +1,26 @@
-import { Children, createContext } from "react";
+import { Children, createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = (props) =>{
+const ShopContextProvider = (props) => {
 
     const currency = '$'
     const delivery_fee = 10;
+    const [search, setSeacrh] = useState('');
+    const [showSearch, setShowSearch] = useState(true)
 
-    const value={
+    const value = {
         products,
         currency,
-        delivery_fee
+        delivery_fee,
+        search,
+        setSeacrh,
+        showSearch,
+        setShowSearch
     }
 
-    return(
+    return (
         <ShopContext.Provider value={value}>
             {props.children}
         </ShopContext.Provider>
